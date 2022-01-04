77d16d5914523735bf69bec9981d93a46850aa1f
Sidinei Silva <sidinei.silva02@gmail.com>
2022-01-03 22:19:38 -0300

    feat: :sparkles: Incluindo middleware de error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 import { usersRoutes } from "./routes/users.routes";
+import { AppError } from "./shared/errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+
+app.use((err: Error, request: Request, response: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      error: err.message,
+    });
+  }
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 export { app };
