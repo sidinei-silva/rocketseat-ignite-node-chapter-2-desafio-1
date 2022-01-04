aeaa687a385959801d3c2d3d42679cd958a36e2e
Sidinei Silva <sidinei.silva02@gmail.com>
2022-01-03 22:14:45 -0300

    feat: :sparkles: Incluindo model User

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,27 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  public readonly id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  created_at?: Date;
+  updated_at?: Date;
+
+  constructor() {
+    if (!this.admin) {
+      this.admin = false;
+    }
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+    if (!this.created_at) {
+      this.created_at = new Date();
+    }
+    if (!this.updated_at) {
+      this.updated_at = new Date();
+    }
+  }
 }
 
 export { User };
